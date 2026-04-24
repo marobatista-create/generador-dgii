eb2e2fe44158da736026a868b162142d63db153b
marobatista <marobatista@gmail.com>
2026-04-23 22:43:03 -0400

    fix: CORS explícito para Vercel

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+}));
+app.options("*", cors());
 app.use(express.json({ limit: "10mb" }));
 
 const DB_CONFIG = {
